9039c18b591e13e6b11d6b8589be1f26737e7449
nick0124 <nikita.popov@lindenvalley.de>
2020-02-01 16:11:55 +0500

    Make level

---
+++ Assets/Scripts/FalingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalingPlatform : MonoBehaviour
+{
+	public float fallTime;
+	private float fallRimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
