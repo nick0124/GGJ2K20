376073415f32394532702935f63a25815d36e6f3
ezheka <efim.zhenya.2000@gmail.com>
2020-02-01 14:05:33 +0500

    scene Jenya
    4 игрока на сцене

--- Assets/Scripts/Jenya/UI_Jenya.cs
+++ Assets/Scripts/Jenya/UI_Jenya.cs
@@ -5,17 +5,81 @@ using UnityEngine.UI;
 
 public class UI_Jenya : MonoBehaviour
 {
-    public static int points;
-    public Text textPoints;
+    public static int points_1, points_2, points_3, points_4;
+    public Text textPoints_1, textPoints_2, textPoints_3, textPoints_4;
+    public GameObject player_1, player_2, player_3, player_4;
+    public int z = 2;
+    //public int windowsInt;
+    public GameObject[] windowsObj;
+    public window[] windows;
+    public static int lastFiksetWindow;
+    int a = -1;
     // Start is called before the first frame update
     void Start()
     {
+        if (z== 2) { textPoints_3.gameObject.SetActive(false); textPoints_4.gameObject.SetActive(false);
+            player_3.SetActive(false); player_4.SetActive(false);
+        }
+        if (z == 3)
+        {
+            textPoints_4.gameObject.SetActive(false);player_4.SetActive(false);
+        }
+                
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        textPoints.text = points.ToString();
+        textPoints_1.text = points_1.ToString();
+        textPoints_2.text = points_2.ToString();
+        textPoints_3.text = points_3.ToString();
+        textPoints_4.text = points_4.ToString();
+
+        bool windowsRepair = true;
+        foreach (var wind in windows)
+        {
+            if (wind.repair == false)
+            {
+                windowsRepair = false;
+            }
+        }
+        if (windowsRepair)
+        {
+            
+            int w = Random.Range(0, windowsObj.Length - 1);
+            if (a != w) {
+                windows[w].breakWindow();
+                a = w;
+            }
+            else
+            {
+                w = Random.Range(0, windowsObj.Length - 1);
+                if (a != w)
+                {
+                    windows[w].breakWindow();
+                    a = w;
+                }
+                else
+                {
+                    w = Random.Range(0, windowsObj.Length - 1);
+                    if (a != w)
+                    {
+                        windows[w].breakWindow();
+                        a = w;
+                    }
+                    else
+                    {
+                        w = Random.Range(0, windowsObj.Length - 1);
+                        if (a != w)
+                        {
+                            windows[w].breakWindow();
+                            a = w;
+                        }
+                    }
+                }
+            }
+            
+        }
     }
 }
--- Assets/Scripts/Jenya/window.cs
+++ Assets/Scripts/Jenya/window.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 
 public class window : MonoBehaviour
 {
+    public bool repair = true;
+    private int players;
     // Start is called before the first frame update
     void Start()
     {
-        
+        players = GameData.PlayerCount;
+        //repair = true;
     }
 
     // Update is called once per frame
@@ -17,12 +20,54 @@ public class window : MonoBehaviour
         
     }
 
+    public void breakWindow()
+    {
+        repair = false;
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
-        {            
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 150, 0, 255);
-            UI_Jenya.points++;
+        
+        if(collision.name == "Player-1")
+        {
+            if (!repair)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                UI_Jenya.points_1++;
+                repair = true;
+            }
+            
+        }
+        if (collision.name == "Player-2")
+        {
+            if (!repair)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                UI_Jenya.points_2++;
+                repair = true;
+            }
+
+        }
+        if (collision.name == "Player-3")
+        {
+            if (!repair)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                UI_Jenya.points_3++;
+                repair = true;
+            }
+
+        }
+        if (collision.name == "Player-4")
+        {
+            if (!repair)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+                UI_Jenya.points_4++;
+                repair = true;
+            }
+
         }
     }
 }
