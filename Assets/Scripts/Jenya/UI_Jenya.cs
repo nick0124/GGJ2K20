4a7f3e903ac4f4ec5bb6d896bd347079211fa563
ezheka <efim.zhenya.2000@gmail.com>
2020-02-01 16:12:26 +0500

    Таймер

--- Assets/Scripts/Jenya/UI_Jenya.cs
+++ Assets/Scripts/Jenya/UI_Jenya.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_Jenya : MonoBehaviour
 {
     public static int points_1, points_2, points_3, points_4;
     public Text textPoints_1, textPoints_2, textPoints_3, textPoints_4;
     public GameObject player_1, player_2, player_3, player_4;
-    public int z = 2;
-    //public int windowsInt;
+    public Text scoreText;
     public GameObject[] windowsObj;
     public window[] windows;
     public static int lastFiksetWindow;
+    bool stopGame = false;
     int a = -1;
-    // Start is called before the first frame update
     void Start()
     {
         if (GameData.PlayerCount== 2) { textPoints_3.gameObject.SetActive(false); textPoints_4.gameObject.SetActive(false);
@@ -24,37 +24,39 @@ public class UI_Jenya : MonoBehaviour
         {
             textPoints_4.gameObject.SetActive(false);player_4.SetActive(false);
         }
-                
-        
+        InvokeRepeating("RunTimer", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(stopGame == true)
+        {
+            CancelInvoke("RunTimer");
+            scoreText.text = "0";
+            Calculation();
+            SceneManager.LoadScene("Finish");
+        }
+        
         textPoints_1.text = points_1.ToString();
         textPoints_2.text = points_2.ToString();
         textPoints_3.text = points_3.ToString();
         textPoints_4.text = points_4.ToString();
 
-        bool windowsRepair = true;
-        foreach (var wind in windows)
+        if (stopGame == false)
         {
-            if (wind.repair == false)
+            bool windowsRepair = true;
+            foreach (var wind in windows)
             {
-                windowsRepair = false;
+                if (wind.repair == false)
+                {
+                    windowsRepair = false;
+                }
             }
-        }
-        if (windowsRepair)
-        {
-            
-            int w = Random.Range(0, windowsObj.Length - 1);
-            if (a != w) {
-                windows[w].breakWindow();
-                a = w;
-            }
-            else
+            if (windowsRepair)
             {
-                w = Random.Range(0, windowsObj.Length - 1);
+
+                int w = Random.Range(0, windowsObj.Length - 1);
                 if (a != w)
                 {
                     windows[w].breakWindow();
@@ -76,10 +78,59 @@ public class UI_Jenya : MonoBehaviour
                             windows[w].breakWindow();
                             a = w;
                         }
+                        else
+                        {
+                            w = Random.Range(0, windowsObj.Length - 1);
+                            if (a != w)
+                            {
+                                windows[w].breakWindow();
+                                a = w;
+                            }
+                        }
                     }
                 }
             }
-            
+        }
+    }            
+
+    void RunTimer()
+    {
+        scoreText.text = (int.Parse(scoreText.text) - 1).ToString();
+        if(scoreText.text == "0") 
+        {
+            stopGame = true;            
+        }
+    }
+
+    void Calculation()
+    {
+        if((points_1 > points_2)&&(points_1 > points_3)&&(points_1 > points_4))
+        {
+            int i = PlayerPrefs.GetInt("Player1");
+            i++;
+            PlayerPrefs.SetInt("Player1", i);
+            Debug.Log("Player1");
+        }
+        if ((points_1 < points_2) && (points_2 > points_3) && (points_2 > points_4))
+        {
+            int i = PlayerPrefs.GetInt("Player2");
+            i++;
+            PlayerPrefs.SetInt("Player2", i);
+            Debug.Log("Player2");
+        }
+        if ((points_3 > points_2) && (points_3 > points_1) && (points_3 > points_4))
+        {
+            int i = PlayerPrefs.GetInt("Player3");
+            i++;
+            PlayerPrefs.SetInt("Player3", i);
+            Debug.Log("Player3");
+        }
+        if ((points_4 > points_2) && (points_4 > points_3) && (points_4 > points_1))
+        {
+            int i = PlayerPrefs.GetInt("Player4");
+            i++;
+            PlayerPrefs.SetInt("Player4", i);
+            Debug.Log("Player4");
         }
     }
 }
