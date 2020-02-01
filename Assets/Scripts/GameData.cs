25cc32ec0d99f385fdb3b22a243c75ba83f57209
devnem0y <devnem0y@gmail.com>
2020-02-01 12:10:19 +0500

    Выбор количества игроков и запуск случайной сцены

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    private static int playerMax = 4;
+    private static int playerMin = 2;
+
+    public static int PlayerCount { get; set; }
+    public static int PlayerMax => playerMax;
+    public static int PlayerMin => playerMin;
+}
---
+++ Assets/Scripts/StarterScenes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class StarterScenes : MonoBehaviour
+{
+    public enum Arrow { LEFT = -1, RIGHT = 1,}
+
+    private Arrow _arrow = Arrow.LEFT;
+
+    [SerializeField] private Text count;
+    [SerializeField] private Button left;
+    [SerializeField] private Button right;
+
+    private void Awake()
+    {
+        GameData.PlayerCount = GameData.PlayerMin;
+        UpdateUI();
+    }
+
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene(Random.Range(1, 2));
+    }
+
+    public void ClickArrow(int dir)
+    {
+        switch (dir)
+        {
+            case (int)Arrow.LEFT:
+                GameData.PlayerCount--;
+                break;
+            case (int)Arrow.RIGHT:
+                GameData.PlayerCount++;
+                break;
+        }
+        
+        UpdateUI();
+    }
+
+    private void SetLabel(string value)
+    {
+        count.text = value;
+    }
+
+    private void UpdateUI()
+    {
+        SetLabel(GameData.PlayerCount.ToString());
+
+        left.interactable = GameData.PlayerCount > GameData.PlayerMin;
+        right.interactable = GameData.PlayerCount < GameData.PlayerMax;
+    }
+}
