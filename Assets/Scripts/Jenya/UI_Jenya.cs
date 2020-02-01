edbbdbbb73f4784fd127bc27b620a3f173edefbe
ezheka <efim.zhenya.2000@gmail.com>
2020-02-01 11:47:49 +0500

    Update scene Jenya

---
+++ Assets/Scripts/Jenya/UI_Jenya.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Jenya : MonoBehaviour
+{
+    public static int points;
+    public Text textPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textPoints.text = points.ToString();
+    }
+}
---
+++ Assets/Scripts/Jenya/window.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class window : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {            
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 150, 0, 255);
+            UI_Jenya.points++;
+        }
+    }
+}
